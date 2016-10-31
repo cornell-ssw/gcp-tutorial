ea6a68bc60ef573762b2205ea4d3f87f819b4838
David Bindel <bindel@cs.cornell.edu>
2016-10-31 17:52:18 -0400

    Update setup.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
-
+sudo apt-get build-essential
 wget https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
 bash Anaconda3-4.2.0-Linux-x86_64.sh
-. ~/.bashrc
-conda install ipython-notebook
 sudo add-apt-repository ppa:staticfloat/juliareleases
 sudo add-apt-repository ppa:staticfloat/julia-deps
 sudo apt-get update
 sudo apt-get install julia
 sudo apt-get install libzmq3-dev
+
+. $HOME/.bashrc
+conda install jupyter
+conda install ipython-notebook
 julia setup.jl
